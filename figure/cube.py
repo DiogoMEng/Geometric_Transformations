23b42019b37214b43ce63898ea99251dc6e4fa3e
DiogoMEng <diogoeng19@gmail.com>
2025-04-01 21:01:01 -0300

    :sparkles: feat: Funcionalidades de translacao, espelhamento e escala adicionadas

--- figure/cube.py
+++ figure/cube.py
@@ -1,41 +1,42 @@
 from OpenGL.GL import *
 
-vertices = [(0.5, -0.5, 0.5),
-            (-0.5, -0.5, 0.5),
-            (0.5, 0.5, 0.5),
-            (-0.5, 0.5, 0.5),
-            (0.5, 0.5, -0.5),
-            (-0.5, 0.5, -0.5),
-            (0.5, -0.5, -0.5),
-            (-0.5, -0.5, -0.5),
-            (0.5, 0.5, 0.5),
-            (-0.5, 0.5, 0.5),
-            (0.5, 0.5, -0.5),
-            (-0.5, 0.5, -0.5),
-            (0.5, -0.5, -0.5),
-            (0.5, -0.5, 0.5),
-            (-0.5, -0.5, 0.5),
-            (-0.5, -0.5, -0.5),
-            (-0.5, -0.5, 0.5),
-            (-0.5, 0.5, 0.5),
-            (-0.5, 0.5, -0.5),
-            (-0.5, -0.5, -0.5),
-            (0.5, -0.5, -0.5),
-            (0.5, 0.5, -0.5),
-            (0.5, 0.5, 0.5),
-            (0.5, -0.5, 0.5)
-            ]
+# Vértices do cubo
+vertices = [
+    (0.5, -0.5, 0.5),   # 0
+    (-0.5, -0.5, 0.5),  # 1
+    (0.5, 0.5, 0.5),    # 2
+    (-0.5, 0.5, 0.5),   # 3
+    (0.5, 0.5, -0.5),   # 4
+    (-0.5, 0.5, -0.5),  # 5
+    (0.5, -0.5, -0.5),  # 6
+    (-0.5, -0.5, -0.5)  # 7
+]
 
-triangles = [0, 2, 3, 0, 3, 1, 8, 4, 5, 8, 5, 9, 10, 6, 7, 10, 7, 11, 12,
-             13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23]
+# Faces do cubo (formadas por 4 vértices)
+faces = [
+    (0, 1, 3, 2),  # Frente
+    (4, 5, 7, 6),  # Trás
+    (2, 3, 5, 4),  # Topo
+    (0, 1, 7, 6),  # Base
+    (1, 3, 5, 7),  # Esquerda
+    (0, 2, 4, 6)   # Direita
+]
 
+# Cores diferentes para cada face
+colors = [
+    (1, 0, 0),  # Vermelho (Frente)
+    (0, 1, 0),  # Verde (Trás)
+    (0, 0, 1),  # Azul (Topo)
+    (1, 1, 0),  # Amarelo (Base)
+    (0, 1, 1),  # Ciano (Esquerda)
+    (1, 0, 1)   # Magenta (Direita)
+]
+
+# Função que desenha o cubo colorido
 def wireCube():
-    for t in range(len(triangles) - 3):
-        glBegin(GL_LINES)
-
-        glVertex3fv(vertices[triangles[t]])
-        glVertex3fv(vertices[triangles[t + 1]])
-        glVertex3fv(vertices[triangles[t + 2]])
-
+    for i, face in enumerate(faces):
+        glColor3fv(colors[i])
+        glBegin(GL_QUADS)
+        for vertex in face:
+            glVertex3fv(vertices[vertex])
         glEnd()
-        t += 3
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from Cube import wireCube
+from figure.cube import wireCube
 
+# Inicialização do Pygame e OpenGL
 pygame.init()
 
 screen_width = 1000
@@ -13,41 +14,88 @@ background_color = (0, 0, 0, 1)
 drawing_color = (1, 1, 1, 1)
 
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('Transformações Geométricas - OpenGL')
 
-pygame.display.set_caption('OpenGL in Python')
+# Variáveis de transformação
+angle = 0
+tx, ty, tz = 0.0, 0.0, -5.0
+scale = 1.0
+mirrorX = 1
+mirrorY = 1
+mirrorZ = 1
 
 def initialise():
-    glClearColor(background_color[0], background_color[1], background_color[2], background_color[3])
-    glColor(drawing_color)
+    glClearColor(*background_color)
+    glColor4f(*drawing_color)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(60, (screen_width / screen_height), 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)                            
-    glTranslate(0, 0, -5)
+    glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    glViewport(0, 0, screen.get_width(), screen.get_height())
     glEnable(GL_DEPTH_TEST)
-    glTranslate(0, 0, -2)
-
+    glViewport(0, 0, screen.get_width(), screen.get_height())
 
 def display():
+    global angle, scale, mirrorX, mirrorY, mirrorZ
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glRotatef(1, 10, 0, 1)
+
+    glLoadIdentity()
+
+    # 1. Translação (posiciona o objeto no espaço)
+    glTranslatef(tx, ty, tz)
+
+    # 2. Escala (inclui espelhamento se valores forem negativos)
+    glScalef(scale * mirrorX, scale * mirrorY, scale * mirrorZ)
+
+    # 3. Rotação (aplicada por último)
+    glRotatef(angle, 1, 1, 0)
+
+    # Desenhar o cubo
     glPushMatrix()
     wireCube()
     glPopMatrix()
 
-
-done = False
-initialise()
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-    display()
-
+    angle += 1
     pygame.display.flip()
 
-    pygame.time.wait(100);
-pygame.quit()
+def main():
+    global tx, ty, tz, scale, mirrorX
+
+    initialise()
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                elif event.key == K_LEFT:
+                    tx -= 0.1
+                elif event.key == K_RIGHT:
+                    tx += 0.1
+                elif event.key == K_UP:
+                    ty += 0.1
+                elif event.key == K_DOWN:
+                    ty -= 0.1
+                elif event.key == K_PAGEUP:
+                    tz += 0.1
+                elif event.key == K_PAGEDOWN:
+                    tz -= 0.1
+                elif event.key == K_PLUS or event.key == K_EQUALS:
+                    scale += 0.1
+                elif event.key == K_MINUS:
+                    scale -= 0.1
+                elif event.key == K_m:
+                    mirrorX *= -1
+
+        display()
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
